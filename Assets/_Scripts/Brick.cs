34dc597398be5881cf5d9347ddf72872ac0d2ba0
Mário Gil <mg_mesquita@hotmail.com>
2017-12-07 21:12:05 +0000

    Uploaded files

---
+++ Assets/_Scripts/Brick.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Brick : MonoBehaviour {
+	
+	public Sprite[] hitSprites;
+	public AudioClip sound;
+	public GameObject smoke;
+	
+	public static int brickCount = 0;
+	
+	private int timesHit;
+	
+	private LevelManager levelManager;
+	
+	void OnCollisionEnter2D(Collision2D collider) {
+		AudioSource.PlayClipAtPoint(sound, transform.position, 0.5f);
+		timesHit++;		
+		int maxHits = hitSprites.Length + 1;
+		
+		if (timesHit >= maxHits && this.tag == "Breakable") {
+			brickCount--;			
+			Instantiate (smoke, gameObject.transform.position, Quaternion.identity);			
+			Destroy (gameObject);}
+		else {
+			LoadSprites (); }
+		if (brickCount <= 0) { NextLevel (); }
+		}
+			
+	void Start () {
+		timesHit = 0;
+		levelManager = GameObject.FindObjectOfType<LevelManager>();
+		if (this.tag == "Breakable") { brickCount++; }
+		}
+
+	void LoadSprites() {
+		int spriteIndex = timesHit - 1;
+		if (hitSprites[spriteIndex]) {
+		this.GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+		} else { Debug.LogError("Sprite missing!"); }
+	}
+		
+	void NextLevel() {
+		levelManager.LoadNextLevel();
+		}
+}
---
+++ Assets/_Scripts/LevelManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name) {
+		Brick.brickCount = 0;
+		Application.LoadLevel(name);
+	}
+	
+	public void LoadNextLevel () {
+		Brick.brickCount = 0;
+		Arrow.start = false;
+		Application.LoadLevel(Application.loadedLevel + 1);
+	}
+	
+	public void QuitRequest () {
+		Application.Quit ();
+	}
+	
+
+}
