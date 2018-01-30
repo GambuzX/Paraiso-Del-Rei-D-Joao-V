10383eed0d5349b5c0e83abe8809d2a67c796afe
Mário Gil <mg_mesquita@hotmail.com>
2018-01-30 09:58:39 +0000

    Game Files

---
+++ Assets/_Scripts/ArrowHP.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArrowHP : MonoBehaviour {
+
+	public int destroyHP;
+
+	void Update () {	
+		if (destroyHP >= LoseCollider.HP) { Destroy(gameObject); } 
+	}
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+Paraiso-Del-Rei-D-Joao-V
+
+Block Breaker kind of game.
+
+The theme is based on a joke from my group of friends, who are actually in the game as blocks.
+
+Download the rar file to play!
